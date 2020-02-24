e48d9657fdfb1f38311a5504f832f18930e464d8
Mathieu Mathias <mathieu.mathias@vedana.com>
2020-02-24 18:14:33 +0100

    few updates

--- src/app/home/constants/messages.js
+++ src/app/home/constants/messages.js
@@ -3,6 +3,7 @@ import {defineMessages} from 'react-intl';
 const messages = defineMessages({
     boxHeader: {id: 'home.homePage.box.header', defaultMessage: 'This a Box Header'},
     boxBody: {id: 'home.homePage.box.body', defaultMessage: 'The content of this Box is simple.'},
+    pageTitle: {id: 'home.homePage.page.title', defaultMessage: 'A simple page title.'}
 });
 
 export default messages;
