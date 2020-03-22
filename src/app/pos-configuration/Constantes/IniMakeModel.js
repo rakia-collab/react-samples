e33704104ce77b12bff5e0649b40357dca0a9d5d
eya <eya.jenhani@soprabanking.com>
2020-03-22 23:30:05 +0100

    init model issue

--- src/app/pos-configuration/Constantes/IniMakeModel.js
+++ src/app/pos-configuration/Constantes/IniMakeModel.js
@@ -1,4 +1,4 @@
 export const dataInit={"makeGeneralData":{"brandRef":null,"countryCode":"null","currencyCode":null},
     "makeOtherData":{"dealer":null,"variantClassification":null,"oemClassification":null},
-    "models":[{}]
+    "models":[]
        }
